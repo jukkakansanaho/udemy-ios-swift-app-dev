74c1e21133eb906c56c1700bf21ac8acfbbb4239
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-05-30 11:35:04 +0300

    S3: Adding I-Am-Poor app

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jukka Kansanaho on 30.5.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
