a2129fa533051a68e3e67d48bbb7d352dcdbab58
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-10 18:06:47 +0300

    S7: Adding Coding Exercise 5 to Playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -35,21 +35,53 @@
 //exercise()
 
 // Coding Exercise 4: Randomization
-func exercise() {
-    
-    
-    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    
-    //The number of letters in alphabet equals 26
-    let a = alphabet[Int.random(in: 0...25)]
-    let b = alphabet[Int.random(in: 0...25)]
-    let c = alphabet[Int.random(in: 0...25)]
-    let d = alphabet[Int.random(in: 0...25)]
-    let e = alphabet[Int.random(in: 0...25)]
-    let f = alphabet[Int.random(in: 0...25)]
-    let password = a + b + c + d + e + f
+//func exercise() {
+//
+//
+//    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//
+//    //The number of letters in alphabet equals 26
+//    let a = alphabet[Int.random(in: 0...25)]
+//    let b = alphabet[Int.random(in: 0...25)]
+//    let c = alphabet[Int.random(in: 0...25)]
+//    let d = alphabet[Int.random(in: 0...25)]
+//    let e = alphabet[Int.random(in: 0...25)]
+//    let f = alphabet[Int.random(in: 0...25)]
+//    let password = a + b + c + d + e + f
+//
+//    print(password)
+//
+//}
+//exercise()
 
-    print(password)
-    
+//Coding Exewrcise 5: Functions
+func calculator() {
+//  let a = Int(readLine()!)! //First input
+//  let b = Int(readLine()!)! //Second input
+    let a = Int(3) //First input
+    let b = Int(4) //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
 }
-exercise()
+
+func add(n1: Int, n2: Int) {
+    print(n1 + n2)
+}
+
+func subtract(n1: Int, n2: Int)  {
+    print(n1 - n2)
+}
+
+func multiply(n1: Int, n2: Int) {
+    print(n1 * n2)
+}
+
+func divide(n1: Int, n2: Int) {
+    print( Double(n1) / Double(n2) )
+}
+
+calculator()
