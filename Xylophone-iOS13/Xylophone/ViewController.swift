edd83d14f61dc3dc35b65510b899f2a797bee4ef
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-10 15:27:45 +0300

    S7: Adding sound playback functionality for C-button

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -1,5 +1,18 @@
+/*
+ NOTE:
+ - Stack Overflow for Audio playback hints: https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
+ - Apple docs for Audio playback:
+    - https://developer.apple.com/documentation/avfoundation/
+    - https://developer.apple.com/documentation/avfoundation/audio_playback_recording_and_processing
+ - Apple Docs for Bundle: https://developer.apple.com/documentation/foundation/bundle/
+ - Apple Docs for AVAudioSession: https://developer.apple.com/documentation/avfaudio/avaudiosession/
+ */
 
 import UIKit
+import AVFoundation
+
+//var player: AVAudioPlayer?
+var player: AVAudioPlayer!
 
 class ViewController: UIViewController {
 
@@ -8,10 +21,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("C-key was pressed...")
+        playSound()
     }
     
+//    NOTE: See below eample code from Stack Overflow:
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
     
-
+//    NOTE: See below (simplified) example code:
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
