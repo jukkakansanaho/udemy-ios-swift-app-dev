d93838989c907923ebe55cf67c0e80cb1f5afc02
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-15 15:43:32 +0300

    S8: Adding Timer to count down the time for each egg hardness

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,27 +7,36 @@ class ViewController: UIViewController {
 //    let mediumTime = 8
 //    let hardTime = 12
     let eggTimes : [String : Int] = [
-        "Soft": 5,
-        "Medium": 8,
-        "Hard": 12
+        "Soft": 5 * 60,
+        "Medium": 8 * 60,
+        "Hard": 12 * 60
     ]
+    var timeRemaining = 60
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         
         let hardness = sender.currentTitle! // Soft | Medium | Hard
         
-//        if hardness == "Soft" {
-//            print(softTime)
-//        } else if hardness == "Medium" {
-//            print(mediumTime)
-//        } else if hardness == "Hard" {
-//            print(hardTime)
-//        } else {
-//            print("Wrong hardness selected...")
+        timeRemaining = eggTimes[hardness]!
+        
+//        // Option-1: Timer with Closure
+//        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+//            if self.timeRemaining > 0 {
+//                print("\(self.timeRemaining) seconds.")
+//                self.timeRemaining -= 1
+//            } else {
+//                timer.invalidate()
+//            }
 //        }
-        let result = eggTimes[hardness]!
-        print(result)
-            
+        // Option-2: Timer with a function
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil,  repeats: true)
+        
     }
     
+    @objc func updateTimer() {
+        print("\(timeRemaining) seconds.")
+        timeRemaining -= 1
+    }
 }
