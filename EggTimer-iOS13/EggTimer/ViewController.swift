86da275a53efecd57b21599ea6915869672afc00
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-15 12:04:25 +0300

    S8: Adding if-else to print out boiling time for the selected egg hardness.

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -3,12 +3,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    @IBAction func hardnessSelected(_ sender: UIButton) {
-//        print(sender.currentTitle)
-//    }
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle)
+        
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(softTime)
+        } else if hardness == "Medium" {
+            print(mediumTime)
+        } else if hardness == "Hard" {
+            print(hardTime)
+        } else {
+            print("Wrong hardness selected...")
+        }
     }
     
 }
