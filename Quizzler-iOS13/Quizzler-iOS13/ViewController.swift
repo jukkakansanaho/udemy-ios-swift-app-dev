b1106a624850c9745dcca299c26b4cb0d65cedeb
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-15 19:00:44 +0300

    S9: Combining questions + checking answers. Adding support for looping the questions.

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -15,16 +15,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One.", "True"],
+        ["Three + Eight is less than Ten.", "False"]
+    ]
+        
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        let userAnswer = sender.currentTitle // True | False
+        let actualAnswer = quiz[questionNumber][1]
+            
+        if userAnswer == actualAnswer {
+            print("RIGHT!")
+        } else {
+            print("WRONG!")
+        }
+        
+        if (questionNumber + 1) < quiz.count {
+            questionNumber += 1
+        } else {
+            print("DONE!")
+            questionNumber = 0
+        }
+        updateUI()
+        
     }
     
-    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
