892c87daa3d150740e481e578a093010d2122cdd
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2021-06-15 17:14:16 +0300

    S8: Adding progresBar (to show timer countdown). Changing titleLabel to show egg hardness.

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,22 +7,25 @@ class ViewController: UIViewController {
 //    let mediumTime = 8
 //    let hardTime = 12
     let eggTimes : [String : Int] = [
-        "Soft": 5 * 60,
-        "Medium": 8 * 60,
-        "Hard": 12 * 60
+        "Soft": 3, //5 * 60,
+        "Medium": 4, //8 * 60,
+        "Hard": 7 //12 * 60
     ]
-    var timeRemaining = 60
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
-        
         let hardness = sender.currentTitle! // Soft | Medium | Hard
-        
-        timeRemaining = eggTimes[hardness]!
-        
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
 //        // Option-1: Timer with Closure
 //        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
 //            if self.timeRemaining > 0 {
@@ -38,16 +41,14 @@ class ViewController: UIViewController {
     }
     
     @objc func updateTimer() {
-        if timeRemaining > 0 {
-            print("\(timeRemaining) seconds.")
-            timeRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print("\(secondsPassed)/\(totalTime) sec. passed.")
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             self.titleLabel.text = "DONE"
             print("DONE")
-            
         }
-        
-        
     }
 }
